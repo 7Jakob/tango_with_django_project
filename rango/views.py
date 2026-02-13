958f5a328612f755d03d29e7b8b300f015a66173
Jakob_Belew <3179714b@student.gla.ac.uk>
2026-02-13 03:09:46 +0000

    Chapter 6 complete - categories, slugs, category pages, index updates, tests passing

--- rango/views.py
+++ rango/views.py
@@ -11,32 +11,33 @@ def decode_url(name):
 
 
 def index(request):
-    category_list = Category.objects.all()
+    category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
 
     context_dict = {
         'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!',
         'categories': category_list,
+        'pages': page_list,
     }
 
     return render(request, 'rango/index.html', context=context_dict)
 
 
 def show_category(request, category_name_slug):
-    category_name = decode_url(category_name_slug)
     context_dict = {}
 
     try:
-        category = Category.objects.get(name=category_name)
+        category = Category.objects.get(slug=category_name_slug)
         pages = Page.objects.filter(category=category)
 
         context_dict['category'] = category
         context_dict['pages'] = pages
-
     except Category.DoesNotExist:
         context_dict['category'] = None
         context_dict['pages'] = None
 
     return render(request, 'rango/category.html', context=context_dict)
+
 
 
 
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from rango import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('rango/', include('rango.urls')),
+    path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
+
 ]
